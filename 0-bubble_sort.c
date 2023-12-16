9f084623f24021902110281dab66bbdd41b212f0
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-16 23:16:34 +0000

    Add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array with Bubble sort algorithm
+ * @array: Array to sort
+ * @size: Array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	while (1)
+	{
+		size_t isSwapped = 0;
+		size_t i = 0;
+
+		while (i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				int tmp = array[i];
+
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				isSwapped++;
+				print_array(array, size);
+			}
+			i++;
+		}
+		if (isSwapped == 0)
+			break;
+	}
+}
